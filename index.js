e0be3d8c56e0c213dfb0646e97822ec921d106a8
Hariom Singh Thakur <hariomthakur638@gmail.com>
2024-03-25 21:55:58 +0530

    finally I came to know how to send responses over requests

--- index.js
+++ index.js
@@ -4,20 +4,6 @@ const app = express();
 
 console.dir(app);
 
-// const info = {
-
-//     profession: "Software engineer",
-//     interestedArea: "Backend Programming,Artificial Intelligence,App development"
-// }
-
-
-// const contact = {
-
-//     linkedin: "hariom Singh Thakur",
-//     github: "www.github.com/hariom-hub",
-//     gmail: "hariomthakur638@gmail.com"
-// }
-
 let port = 3000;
 // let port2 = 5000;
 
@@ -32,15 +18,59 @@ app.listen(port, () => {
 //     console.log(`app is listening on a port ${port2}`);
 // })
 
-app.use((req, res) => {
-    // console.log(res);
-    // console.log(req);
-    res.send({
-        error_type: "network error",
-        connection_speed: "10mb/s",
-        status: "request denied"
+// app.use((req, res) => {
+//     // console.log(res);
+//     // console.log(req);
+//     res.send({
+//         error_type: "network error",
+//         connection_speed: "10mb/s",
+//         status: "request denied"
 
-    })
-    console.log("request received");
+//     })
+//     console.log("request received");
+// });
+
+const about = {
+
+    name: "hariom",
+    age: 20,
+    height: 5.7,
+    education: "B.tech in Cse",
+    profession: "Software engineer",
+    interestedArea: "Backend Programming,Artificial Intelligence,App development"
+}
+
+const contact = {
+
+    linkedin: "hariom Singh Thakur",
+    github: "www.github.com/hariom-hub",
+    gmail: "hariomthakur638@gmail.com"
+}
+
+
+app.get("/", (req, res) => {
+
+    res.send("Root path :-) ");
 });
 
+app.get("/child", (req, res) => {
+
+    res.send("Child Path :-> ");
+});
+
+
+app.get("/about", (req, res) => {
+
+    res.send(about);
+});
+
+app.get("/contact",(req,res)=>{
+
+    res.send(contact);
+});
+
+
+app.get("/file",(req,res)=>{
+
+    res.sendFile(__dirname+'/index.html');
+});
