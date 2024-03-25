a71437eb70abf5a202a96e50a159709b3c9af602
Hariom Singh Thakur <hariomthakur638@gmail.com>
2024-03-25 21:37:26 +0530

    files saved

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
+
 const express = require("express");
 const app = express();
 
 console.dir(app);
 
+// const info = {
+
+//     profession: "Software engineer",
+//     interestedArea: "Backend Programming,Artificial Intelligence,App development"
+// }
+
+
+// const contact = {
+
+//     linkedin: "hariom Singh Thakur",
+//     github: "www.github.com/hariom-hub",
+//     gmail: "hariomthakur638@gmail.com"
+// }
 
 let port = 3000;
-let port2 = 5000;
+// let port2 = 5000;
 
 
 app.listen(port, () => {
@@ -19,10 +33,14 @@ app.listen(port, () => {
 // })
 
 app.use((req, res) => {
+    // console.log(res);
+    // console.log(req);
+    res.send({
+        error_type: "network error",
+        connection_speed: "10mb/s",
+        status: "request denied"
 
-
-
-    console.log(res);
-    console.log(req);
+    })
     console.log("request received");
 });
+
